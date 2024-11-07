aa324851859d689ade418e16053d326205b7126a
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-11-06 20:31:40 -0800

    Test passing

--- src/leetcode/fzakaria/MergeSortedArray.java
+++ src/leetcode/fzakaria/MergeSortedArray.java
@@ -1,4 +1,31 @@
 package leetcode.fzakaria;
 
+/**
+ * @link https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+ */
 public class MergeSortedArray {
+
+    public static void merge(int[] lhs, int m , int[] rhs, int n) {
+        assert lhs.length == m + n;
+        assert rhs.length == n;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            // if lhs is smaller than rhs do nothing
+            if (lhs[i] <= rhs[j]) {
+                i++;
+            } else {
+                // swap it with the start of the rhs
+                int temp = lhs[i];
+                lhs[i] = rhs[j];
+                rhs[j] = temp;
+                j++;
+            }
+        }
+
+        System.arraycopy(rhs, 0, lhs, m, n);
+    }
+
 }
---
+++ src/leetcode/fzakaria/MergeSortedArrayTest.java
@@ -0,0 +1,23 @@
+package leetcode.fzakaria;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MergeSortedArrayTest {
+
+    @Test
+    public void basicTest() {
+        int[] lhs = new int[]{1, 2, 3, 0, 0, 0};
+        int[] rhs = new int[]{2, 5, 6};
+        MergeSortedArray.merge(lhs, 3, rhs, 3);
+        assertThat(lhs).containsExactly(1, 2, 2, 3, 5, 6);
+    }
+
+    @Test
+    public void advancedTest() {
+        int[] lhs = new int[]{4, 5, 6, 0, 0, 0};
+        int[] rhs = new int[]{1, 2, 3};
+        MergeSortedArray.merge(lhs, 3, rhs, 3);
+        assertThat(lhs).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+}
