b9f8f9aad4585f4493fa7762adc093d12a6017cc
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-11-06 19:58:09 -0800

    Bazel and Nix scaffolding

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+        ];
+      });
+  in {
+    formatter = forAllSystems (system: (nixpkgsFor.${system}).alejandra);
+
+    devShells = forAllSystems (system:
+      with nixpkgsFor.${system}; {
+        default = mkShellNoCC {
+          packages = [
+            bazel_7
+          ];
+        };
+      });
+  };
+}
